f55bd7f652c377b7430b7472be1b4e62cc9148b1
Codey <coxley@users.noreply.github.com>
2014-12-04 22:30:07 -0500

    Added socket for pickle xfer. Can now send to carbon.
    example.conf updated to reflect necessary configuration option changes.

--- src/SNMPPoll/logger.py
+++ src/SNMPPoll/logger.py
@@ -6,7 +6,8 @@ import logging.handlers
 format = logging.Formatter('%(asctime)s: %(message)s')
 log_file = os.path.expanduser('~/.snmp-poller/snmp-poller.log')
 logger = logging.getLogger('snmp-poller')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.INFO)
 handler = logging.handlers.RotatingFileHandler(
     log_file,
     maxBytes=500000,
--- src/SNMPPoll/snmppoll.py
+++ src/SNMPPoll/snmppoll.py
@@ -1,25 +1,42 @@
 import time
-import logging
+import socket
+import struct
+import pickle
 from snimpy.manager import Manager
 from snimpy.manager import load
 from configobj import ConfigObj
-import logger
+import SNMPPoll.logger
 
-log = logger.logger
-timestamp = time.time()
-config_path = '/etc/snmp-poller/devices.conf'
+TIMESTAMP = time.time()
+CONFIG_PATH = '/etc/snmp-poller/devices.conf'
+
+log = SNMPPoll.logger.logger
+
 
 def get_config(path):
+    '''Return dict created from ConfigObj
+    :param path: path to config file
+    :param type: str
+    '''
     return ConfigObj(path)
 
 
 def poll_device(ip, snmp_community, snmp_version, path):
+    '''Using SNMP, polls single device for ifMIB returning tuple for graphite.
+    :param ip: ip of the device
+    :type ip: str
+    :param snmp_community: SNMP community that allows ifMIB values to be read
+    :type snmp_community: str
+    :param snmp_version: version of SNMP device supports
+    :type snmp_version: int
+    :param path: desired graphite path for device
+    :type path: str
+    '''
     null_ifs = [
         'Null0',
         'NVI0',
         ]
     pickle_tuples = []
-    snmp_version = int(snmp_version)
     if snmp_version == 2:
         load('SNMPv2-MIB')
         load('IF-MIB')
@@ -36,28 +53,66 @@ def poll_device(ip, snmp_community, snmp_version, path):
             out_octets = int(m.ifHCOutOctets[iface])
             in_octets = int(m.ifHCInOctets[iface])
             log.debug('%s: out_octets: %s, in_octets: %s',
-                          m.ifDescr[iface], out_octets, in_octets)
-            pickle_tuples.append((path_out, (timestamp, out_octets)))
-            pickle_tuples.append((path_in, (timestamp, in_octets)))
+                      m.ifDescr[iface], out_octets, in_octets)
+            pickle_tuples.append((path_out, (TIMESTAMP, out_octets)))
+            pickle_tuples.append((path_in, (TIMESTAMP, in_octets)))
     return pickle_tuples
 
 
-def pickle_all(config=get_config(config_path)):
-    all_pickle = []
+def pickle_all(config=get_config(CONFIG_PATH)):
+    '''Creates pickle for each device configured and calls send_pickle()
+    :param config: configuration options for devices
+    :param type: dict
+    '''
+    SERVER = (config['PICKLE']['SERVER'], int(config['PICKLE']['PORT']))
     for section in config:
-        for subsection in config[section]:
-            sub = config[section][subsection]
-            path = sub['GRAPHITE_PATH']
-            ip = sub['IP']
-            snmp_community = sub['SNMP_COMMUNITY']
-            snmp_version = sub['SNMP_VERSION']
-            log.info('Beginning poll of device: %s', ip)
-            pickles = poll_device(ip, snmp_community, snmp_version, path)
-            log.info('Finished polling device: %s', ip)
-            all_pickle.append(pickles)
-    return all_pickle
+        if section != 'PICKLE':
+            for subsection in config[section]:
+                sub = config[section][subsection]
+                path = sub['GRAPHITE_PATH']
+                ip = sub['IP']
+                snmp_community = sub['SNMP_COMMUNITY']
+                snmp_version = int(sub['SNMP_VERSION'])
+                log.info('Beginning poll of device: %s', ip)
+                pickles = poll_device(ip, snmp_community, snmp_version, path)
+                log.info('Finished polling device: %s', ip)
+                send_pickle(SERVER, pickles)
+    return True
+
+
+def send_pickle(server, pre_pickle):
+    '''Open socket and send pickle as packet.
+    :param server: server and port to connect to
+    :type server: tuple with server as str and port as int
+    :param pre_pickle: message to send as payload
+    :type pre_pickle: any type to be pickled
+    '''
+
+    payload = pickle.dumps(pre_pickle, protocol=2)
+    header = struct.pack('!L', len(payload))
+    message = header + payload
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    log.debug('Connecting to %s:%d' % server)
+    sock.connect(server)
+    try:
+        message = ''
+        log.debug('Beginning data xfer to %s:%d' % server)
+        sock.sendall(message)
+
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            log.debug('Received %d bytes', len(data))
+    finally:
+        log.debug('Xfer completed. Closing socket on %s:%d' % server)
 
 
 def run():
-    pickles = pickle_all()
-    return pickles
+    '''Initiate the process.
+    :params: none
+    '''
+    return pickle_all()
